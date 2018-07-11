d77b7792c46ce04b468d6c5a100152a7990d81ee
ARoss1216 <a.ross24852@gmail.com>
2018-07-11 18:36:33 +0000

    automatically backed up by learn

---
+++ ashley.rb
@@ -0,0 +1,19 @@
+celebCrushes = ["Beyonce", "Rihanna", "Ozuna"]
+  
+  puts celebCrushes
+  
+  name = gets.chop
+  
+  puts "What is the new #{name}?"
+  
+  
+  
+  celebCrushes << #{name}
+  
+celebCrushes
+  
+  
+  
+  
+  
+  
